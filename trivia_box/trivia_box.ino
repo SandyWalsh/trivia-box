afe83b441c0afb0bd5e272cd570d465bef3e4430
Sandy Walsh <sandy@sandywalsh.com>
2013-11-16 16:25:27 -0400

    Reliable latch and reset. Team/player models

--- trivia_box/trivia_box.ino
+++ trivia_box/trivia_box.ino
@@ -19,18 +19,43 @@ void setup() {
       digitalWrite(pin, HIGH);
     }
   }
+  
+  // Use analog 0 pullup-resistor for faster resets.
+  digitalWrite(A0, HIGH);
 }
 
 void loop() {
-  for (int team=0; team < NUM_TEAMS; team++) {
-    for (int player=0; player < PLAYERS_PER_TEAM; player++) {
-      int pin = teams[team][player];
-      int state = digitalRead(pin);
-      Serial.print(pin);
-      Serial.print(" ");
-      Serial.println(state);
+  Serial.println("--------- Ready --------------------");  
+
+  while (analogRead(14) < 100) 
+    ;
+    
+  Serial.println("----------- GO! -------------");
+
+  int winning_team = -1;
+  int winning_player = -1;
+
+  while (winning_team == -1) {    
+    for (int team=0; team < NUM_TEAMS && winning_team == -1; team++) {
+      for (int player=0; player < PLAYERS_PER_TEAM && winning_player == -1; player++) {
+        int pin = teams[team][player];
+        int state = digitalRead(pin);
+
+        if (state == 0) {
+           winning_team = team;
+           winning_player = player;
+        }
+      }
     }
   }
-  delay(500);
-  Serial.println("-----------------------------");
+  
+  Serial.print("Winning Team: "); 
+  Serial.print(winning_team);
+  Serial.print(", Winning Player: ");
+  Serial.println(winning_player);
+  Serial.println("--------- Waiting for Reset ---------------");  
+  
+  while (analogRead(14) >= 100)
+    ;
+
 }
